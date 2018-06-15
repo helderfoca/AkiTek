c6efd4baa66f3018f169dccbcf9dd4cf6e03ebee
Helder Lopes <aluno19784@ipt.pt>
2018-06-15 07:38:43 +0100

    Seed para 2 produtos HiddenFor() para chaves estrangeiras

--- AkiTek/Migrations/Configuration.cs
+++ AkiTek/Migrations/Configuration.cs
@@ -42,36 +42,16 @@ namespace AkiTek.Migrations
                 new Caracteristicas {ID=6, Nome="Chipset"},
                 new Caracteristicas {ID=7, Nome="Processador"},
                 new Caracteristicas {ID=8, Nome="Gráficos"},
-                //new Caracteristicas {ID=9, Nome="Armazenamento",  Descricao="16GB - expansível via microSD"},
-                //new Caracteristicas {ID=10, Nome="Memória RAM", Descricao="2GB"},
-                //new Caracteristicas {ID=11, Nome="Ecrã", Descricao="Tipo: Touchscreen capacitivo, S-IPS LCD, 16 Milhões de Cores " +
-                //                                                    "Tamanho: 18:9, 720 x 1440 pixels, 5.7 polegadas(~282 ppi)"},
-                //new Caracteristicas {ID=12, Nome="Câmaras", Descricao="Câmara traseira: 13 MP, phase detection autofocus, LED flash" +
-                //                                                    "Funcionalidades: Geo - tagging, touch focus, face detection, HDR, panorama" +
-                //                                                    "  Vídeo: 1080p" +
-                //                                                    "  Câmara frontal: 5 MP, LED flash"},
-                //new Caracteristicas {ID=13, Nome="Dados", Descricao="WLAN: Wi - Fi 802.11 b / g / n, Wi - Fi Direct, hotspot" +
-                //                                                    "Bluetooth: v4.2, A2DP, LE" +
-                //                                                    "USB: microUSB v2.0"},
-                //new Caracteristicas {ID=14, Nome="Sensores", Descricao="Acelerômetro, proximidade"},
-                //new Caracteristicas {ID=15, Nome="GPS", Descricao="A-GPS, GLONASS"},
-                //new Caracteristicas {ID=16, Nome="Bateria",  Descricao="Não removível Li-Ion 3000 mAh"},
-                //new Caracteristicas {ID=17, Nome="Dimensões", Descricao="152.4 x 73 x 7.8 mm"},
-                //new Caracteristicas {ID=18, Nome="Peso", Descricao="150 g"},
-                
-                //new Caracteristicas {ID=24, Nome="Armazenamento",  Descricao="32GB"},
-                //new Caracteristicas {ID=25, Nome="Ecrã", Descricao="Tamanho: 4.7 polegadas IPS LCD" +
-                //                                                    "Resolução: 750 x 1334(~326 ppi)"},
-                //new Caracteristicas {ID=26, Nome="Câmaras", Descricao="Câmara traseira: 12 MP, f/1.8, 28mm, phase detection autofocus, OIS, quad-LED (dual tone) flash" +
-                //                                                    "Características: 1 / 3'' sensor size, geo-tagging, simultaneous 4K video and 8MP image recording, touch focus, face/smile detection, HDR (photo/panorama)" +
-                //                                                    "Vídeo: 2160p@30fps, 1080p@30 / 60 / 120fps, 720p@240fps" +
-                //                                                    "Câmara frontal: 7 MP, f / 2.2, 32mm, 1080p@30fps, 720p@240fps, face detection, HDR, panorama"},
-                //new Caracteristicas {ID=27, Nome="Dados", Descricao="WLAN: Wi-Fi 802.11 a/b/g/n/ac, dual-band, hotspot" +
-                //                                                    "Bluetooth: 4.2, A2DP, LE"},
-                //new Caracteristicas {ID=28, Nome="Sensores", Descricao="Impressão digital (frontal), acelerômetro, giroscópio, proximidade, bússola, barómetro"},
-                //new Caracteristicas {ID=29, Nome="Bateria", Descricao="Não removível Li-Ion 1960 mAh"},
-                //new Caracteristicas {ID=30, Nome="Dimensões", Descricao="138.3 x 67.1 x 7.1 mm"},
-                //new Caracteristicas {ID=31, Nome="Peso", Descricao="138 g"},
+                new Caracteristicas {ID=9, Nome="Armazenamento"},
+                new Caracteristicas {ID=10, Nome="Memória RAM"},
+                new Caracteristicas {ID=11, Nome="Ecrã"},
+                new Caracteristicas {ID=12, Nome="Câmaras"},
+                new Caracteristicas {ID=13, Nome="Dados"},
+                new Caracteristicas {ID=14, Nome="Sensores"},
+                new Caracteristicas {ID=15, Nome="GPS"},
+                new Caracteristicas {ID=16, Nome="Bateria"},
+                new Caracteristicas {ID=17, Nome="Dimensões"},
+                new Caracteristicas {ID=18, Nome="Peso"},
             };
             caracteristicas.ForEach(aa => context.Caracteristicas.AddOrUpdate(a => a.Nome, aa));
             context.SaveChanges();
@@ -97,21 +77,68 @@ namespace AkiTek.Migrations
             // adiciona 
             var desc = new List<DescricaoCaracteristica> {
                 new DescricaoCaracteristica {ID=1, Descricao="Livre", ProdutoFK=1, CaracteristicaFK=1 },
-                new DescricaoCaracteristica {ID=2, Descricao="Livre", ProdutoFK=2, CaracteristicaFK=1 },
-                new DescricaoCaracteristica {ID=3, Descricao="Sim", ProdutoFK=1, CaracteristicaFK=2 },
-                new DescricaoCaracteristica {ID=4, Descricao="Não", ProdutoFK=2, CaracteristicaFK=2 },
-                new DescricaoCaracteristica {ID=5, Descricao="Nano SIM", ProdutoFK=1, CaracteristicaFK=3 },
-                new DescricaoCaracteristica {ID=6, Descricao="Nano SIM", ProdutoFK=2, CaracteristicaFK=3 },
-                new DescricaoCaracteristica {ID=7, Descricao="4G LTE", ProdutoFK=1, CaracteristicaFK=4 },
-                new DescricaoCaracteristica {ID=8, Descricao="4G LTE", ProdutoFK=2, CaracteristicaFK=4 },
-                new DescricaoCaracteristica {ID=9, Descricao="Android OS, v8.0 (Oreo)", ProdutoFK=1, CaracteristicaFK=5 },
-                new DescricaoCaracteristica {ID=10, Descricao="Apple iOS", ProdutoFK=2, CaracteristicaFK=5 },
-                new DescricaoCaracteristica {ID=11, Descricao="Qualcomm MSM8917 Snapdragon 425", ProdutoFK=1, CaracteristicaFK=6 },
-                new DescricaoCaracteristica {ID=12, Descricao="Apple A10 Fusion", ProdutoFK=2, CaracteristicaFK=6 },
-                new DescricaoCaracteristica {ID=13, Descricao="Quad-core 1.4 GHz Cortex-A53", ProdutoFK=1, CaracteristicaFK=7 },
-                new DescricaoCaracteristica {ID=14, Descricao="Quad-core 2.34 GHz (2x Hurricane + 2x Zephyr)", ProdutoFK=2, CaracteristicaFK=7 },
-                new DescricaoCaracteristica {ID=15, Descricao="Adreno 308", ProdutoFK=1, CaracteristicaFK=8 },
-                new DescricaoCaracteristica {ID=16, Descricao="PowerVR Series7XT Plus (six-core graphics)", ProdutoFK=2, CaracteristicaFK=8 },
+                new DescricaoCaracteristica {ID=2, Descricao="Sim", ProdutoFK=1, CaracteristicaFK=2 },
+                new DescricaoCaracteristica {ID=3, Descricao="Nano SIM", ProdutoFK=1, CaracteristicaFK=3 },
+                new DescricaoCaracteristica {ID=4, Descricao="4G LTE", ProdutoFK=1, CaracteristicaFK=4 },
+                new DescricaoCaracteristica {ID=5, Descricao="Android OS, v8.0 (Oreo)", ProdutoFK=1, CaracteristicaFK=5 },
+                new DescricaoCaracteristica {ID=6, Descricao="Qualcomm MSM8917 Snapdragon 425", ProdutoFK=1, CaracteristicaFK=6 },
+                new DescricaoCaracteristica {ID=7, Descricao="Quad-core 1.4 GHz Cortex-A53", ProdutoFK=1, CaracteristicaFK=7 },
+                new DescricaoCaracteristica {ID=8, Descricao="Adreno 308", ProdutoFK=1, CaracteristicaFK=8 },
+                new DescricaoCaracteristica {ID=9, Descricao="16GB - expansível via microSD", ProdutoFK=1, CaracteristicaFK=9 },
+                new DescricaoCaracteristica {ID=10, Descricao="2GB", ProdutoFK=1, CaracteristicaFK=10 },
+                new DescricaoCaracteristica {ID=11, Descricao="Tipo: Touchscreen capacitivo, S-IPS LCD, 16 Milhões de Cores " +
+                                                                "Tamanho: 18:9, 720 x 1440 pixels, 5.7 polegadas(~282 ppi)",
+                    ProdutoFK =1, CaracteristicaFK=11 },
+                new DescricaoCaracteristica {ID=12, Descricao="Câmara traseira: 13 MP, phase detection autofocus, LED flash" +
+                                                                "Funcionalidades: Geo - tagging, touch focus, face detection, HDR, panorama" +
+                                                                "Vídeo: 1080p" +
+                                                                "Câmara frontal: 5 MP, LED flash",
+                    ProdutoFK =1, CaracteristicaFK=12 },
+                new DescricaoCaracteristica {ID=13, Descricao="WLAN: Wi - Fi 802.11 b / g / n, Wi - Fi Direct, hotspot" +
+                                                                "Bluetooth: v4.2, A2DP, LE" +
+                                                                "USB: microUSB v2.0",
+                    ProdutoFK =1, CaracteristicaFK=13 },
+                new DescricaoCaracteristica {ID=14, Descricao="Acelerômetro, proximidade",
+                    ProdutoFK =1, CaracteristicaFK=14 },
+                new DescricaoCaracteristica {ID=15, Descricao="A-GPS, GLONASS",
+                    ProdutoFK =1, CaracteristicaFK=15 },
+                new DescricaoCaracteristica {ID=16, Descricao="Não removível Li-Ion 3000 mAh",
+                    ProdutoFK =1, CaracteristicaFK=16 },
+                new DescricaoCaracteristica {ID=17, Descricao="152.4 x 73 x 7.8 mm",
+                    ProdutoFK =1, CaracteristicaFK=17 },
+                new DescricaoCaracteristica {ID=18, Descricao="150 g",
+                    ProdutoFK =1, CaracteristicaFK=18 },
+                new DescricaoCaracteristica {ID=19, Descricao="Livre", ProdutoFK=2, CaracteristicaFK=1 },
+                new DescricaoCaracteristica {ID=20, Descricao="Não", ProdutoFK=2, CaracteristicaFK=2 },
+                new DescricaoCaracteristica {ID=21, Descricao="Nano SIM", ProdutoFK=2, CaracteristicaFK=3 },
+                new DescricaoCaracteristica {ID=22, Descricao="4G LTE", ProdutoFK=2, CaracteristicaFK=4 },
+                new DescricaoCaracteristica {ID=23, Descricao="Apple iOS", ProdutoFK=2, CaracteristicaFK=5 },
+                new DescricaoCaracteristica {ID=24, Descricao="Apple A10 Fusion", ProdutoFK=2, CaracteristicaFK=6 },
+                new DescricaoCaracteristica {ID=25, Descricao="Quad-core 2.34 GHz (2x Hurricane + 2x Zephyr)", ProdutoFK=2, CaracteristicaFK=7 },
+                new DescricaoCaracteristica {ID=26, Descricao="PowerVR Series7XT Plus (six-core graphics)", ProdutoFK=2, CaracteristicaFK=8 },
+                new DescricaoCaracteristica {ID=27, Descricao="32GB", ProdutoFK=2, CaracteristicaFK=9 },
+                new DescricaoCaracteristica {ID=28, Descricao="2GB", ProdutoFK=2, CaracteristicaFK=10 },
+                new DescricaoCaracteristica {ID=29, Descricao="Tamanho: 4.7 polegadas IPS LCD" +
+                                                                "Resolução: 750 x 1334(~326 ppi)",
+                    ProdutoFK =2, CaracteristicaFK=11 },
+                new DescricaoCaracteristica {ID=30, Descricao="Câmara traseira: 12 MP, f/1.8, 28mm, phase detection autofocus, OIS, quad-LED (dual tone) flash" +
+                                                                "Características: 1 / 3'' sensor size, geo-tagging, simultaneous 4K video and 8MP image recording, touch focus, face/smile detection, HDR (photo/panorama)" +
+                                                                "Vídeo: 2160p@30fps, 1080p@30 / 60 / 120fps, 720p@240fps" +
+                                                                "Câmara frontal: 7 MP, f / 2.2, 32mm, 1080p@30fps, 720p@240fps, face detection, HDR, panorama",
+                    ProdutoFK =2, CaracteristicaFK=12 },
+                new DescricaoCaracteristica {ID=31, Descricao="WLAN: Wi-Fi 802.11 a/b/g/n/ac, dual-band, hotspot" +
+                                                                "Bluetooth: 4.2, A2DP, LE",
+                    ProdutoFK =2, CaracteristicaFK=13 },
+                new DescricaoCaracteristica {ID=32, Descricao="Impressão digital (frontal), acelerômetro, giroscópio, proximidade, bússola, barómetro",
+                    ProdutoFK =2, CaracteristicaFK=14 },
+                new DescricaoCaracteristica {ID=33, Descricao="A-GPS, GLONASS",
+                    ProdutoFK =2, CaracteristicaFK=15 },
+                new DescricaoCaracteristica {ID=34, Descricao="Não removível Li-Ion 1960 mAh",
+                    ProdutoFK =2, CaracteristicaFK=16 },
+                new DescricaoCaracteristica {ID=35, Descricao="138.3 x 67.1 x 7.1 mm",
+                    ProdutoFK =2, CaracteristicaFK=17 },
+                new DescricaoCaracteristica {ID=36, Descricao="138 g",
+                    ProdutoFK =2, CaracteristicaFK=18 },
             };
             desc.ForEach(aa => context.DescricaoCaracteristica.AddOrUpdate(a => a.ID, aa));
             context.SaveChanges();
